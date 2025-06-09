6fe7316721d7d1fbdb8d5d553333516ace887f1d
alafgani1000 <irhabyaganal625@gmail.com>
2025-06-10 05:05:27 +0700

    add style

--- resources/js/Pages/QuestionTypes/MultipleResponse.jsx
+++ resources/js/Pages/QuestionTypes/MultipleResponse.jsx
@@ -1,17 +1,18 @@
 export default function MultipleResponse({ question }) {
     return (
-        <div key={question.id} className="text-xl">
-            <div>{question.question} ?</div>
-            <ul className="list-none mt-4 ms-8">
+        <div key={question.id}>
+            <div className="text-xl font-semibold">{question.question} ?</div>
+            <ul className="list-none mt-8 ms-2 text-base space-y-2">
                 {question.answers.map((answer, ianswer) => {
                     return (
-                        <li key={answer.id}>
+                        <li key={answer.id} className="border flex items-center px-2 py-2 rounded">
                             <input
                                 type="checkbox"
-                                className="me-2"
+                                className="text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                                 name={question.id}
+                                id={answer.id}
                             />
-                            {answer.content}
+                            <label htmlFor={answer.id} className="w-full ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">{answer.content}</label>
                         </li>
                     );
                 })}
--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -8,7 +8,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
         <>
             <Head title="Welcome" />
             <div className="flex items-center justify-center h-screen bg-white dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-                <div className="xl:w-3/5 lg:w-full md:w-full w-full md:mx-4 mx-4 border rounded-lg py-5">
+                <div className="xl:w-3/5 lg:w-full md:w-full w-full md:mx-4 mx-4 border shadow rounded-lg py-5">
                     <div className="flex px-6 justify-between">
                         <div className="text-center text-sm sm:text-start">
                             1 from 50
@@ -21,7 +21,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                         {/* multiple choice */}
                         {questions.map((question, index) => {
                             return (
-                                <MultipeChoice
+                                <MultipleResponse
                                     question={question}
                                     key={question.id}
                                 />
@@ -30,10 +30,10 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                     </div>
 
                     <div className="flex justify-center mt-8 px-6 space-x-8">
-                        <div className="text-center text-sm sm:text-start">
+                        <div className="text-center text-sm sm:text-start bg-blue-500 px-8 py-2 text-white rounded-full">
                             Prev
                         </div>
-                        <div className="text-center text-sm sm:text-start">
+                        <div className="text-center text-sm sm:text-start bg-blue-500 px-8 py-2 text-white rounded-full">
                             Next
                         </div>
                     </div>
